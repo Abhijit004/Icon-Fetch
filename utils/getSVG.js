c5fc26dd6ea1f4b2958dfb2337af9769ee53e0b3
Abhijit <artistrup07@gmail.com>
2025-04-08 21:17:08 +0530

    [UPDATE]: Returning only the icon

--- utils/getSVG.js
+++ utils/getSVG.js
@@ -1,19 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 exports.generateSVG = (iconName) => {
     try {
         // Read the SVG file contents as string
         const svgPath = path.join(__dirname, `../TechIcons/${iconName.toLowerCase()}.svg`);
-        const iconCode = fs.readFileSync(svgPath, 'utf-8');
+        const iconCode = fs.readFileSync(svgPath, "utf-8");
 
         // Create the template using the iconCode and iconName
-        const template = `
-            <div style="display: inline-flex; align-items: center; gap: 45px; background: #1D1F23; border: 1px solid #ccc; border-radius: 35px; padding: 20px 40px; transform: scale(0.3); transform-origin: top left;">
-                ${iconCode}
-                <span style="font-family: monospace; font-size: 60px; color: #fff;text-transform: uppercase">${iconName.toLowerCase()}</span>
-            </div>
-        `;
+        // const template = `
+        //     <div style="display: inline-flex; align-items: center; gap: 45px; background: #1D1F23; border: 1px solid #ccc; border-radius: 35px; padding: 20px 40px; transform: scale(0.3); transform-origin: top left;">
+        //         ${iconCode}
+        //         <span style="font-family: monospace; font-size: 60px; color: #fff;text-transform: uppercase">${iconName.toLowerCase()}</span>
+        //     </div>
+        // `;
+
+        const template = iconCode.replace(`width="100"`, `width="50"`).replace(`height="100"`, `height="50"`);
 
         return template;
     } catch (err) {
